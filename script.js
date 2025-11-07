dc05c70e10531c42c2caa1a10819effb180c9b02
Nisha Bukke <sononenisha827@gmail.com>
2025-11-07 16:59:22 +0530

    small changes

--- script.js
+++ script.js
@@ -142,39 +142,45 @@ questionButton.forEach((elem)=>{
 })
 
 
+//mobile menu
 
-        // mobile menu
-            $( document ).ready(function() {
+        $(document).ready(function () {
+  $(".cross").hide();
+  $(".menu").hide();
 
-            $( ".cross" ).hide();
-            $( ".menu" ).hide();
-            $( ".canva_expander" ).click(function() {
-            $( ".menu" ).slideToggle( "slow", function() {
-            $( ".canva_expander" ).hide();
-            $( ".cross" ).show();
-            });
-            });
+  $(".canva_expander").click(function () {
+    $(".menu").slideToggle("slow", function () {
+      $(".canva_expander").hide();
+      $(".cross").show();
+    });
+  });
 
-            $( ".cross" ).click(function() {
-            $( ".menu" ).slideToggle( "slow", function() {
-            $( ".cross" ).hide();
-            $( ".canva_expander" ).show();
-            });
-            });
+  $(".cross").click(function () {
+    $(".menu").slideToggle("slow", function () {
+      $(".cross").hide();
+      $(".canva_expander").show();
+    });
+  });
 
-            });
+  // Close menu when a link is clicked
+  $(".menu ul li a").click(function () {
+    $(".menu").slideUp("slow", function () {
+      $(".cross").hide();
+      $(".canva_expander").show();
+    });
+  });
 
-            // Close out sub menu
-            $('.sub__close').click(function(e) {
-            e.preventDefault();
+  // Close out sub menu
+  $(".sub__close").click(function (e) {
+    e.preventDefault();
+    $(this).parent().parent().removeClass("is-active");
+  });
 
-            $(this).parent().parent().removeClass('is-active');
-            });
+  // Trigger sub menu
+  $(".menu ul .nav__submenu").click(function (e) {
+    e.preventDefault();
+    $(this).siblings().addClass("is-active");
+  });
+});
 
-            // Trigger sub menu
-            $('.menu ul .nav__submenu').click(function(e) {
-        e.preventDefault();
-
-        $(this).siblings().addClass('is-active');
-        });
   
